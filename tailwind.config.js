ef5f7ed99dad5f36feafcf4a9867bc3ef332513d
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-11-02 16:37:27 +0100

    istemato butto header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-undef
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/react/utils/withMT";
 
@@ -5,9 +8,13 @@ export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 });
+
