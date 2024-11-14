b8a31b00c118f675c18e7b8109794e3a2fe55e36
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-11-14 10:33:16 +0100

    prova deploy 3

--- src/main.jsx
+++ src/main.jsx
@@ -5,33 +5,38 @@ import App from './App'
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom";
+} from 'react-router-dom'
 
 import './index.css'
-import Docs from './components/pages/Docs';
-import About from './components/pages/About';
+import Docs from './components/pages/Docs'
+import About from './components/pages/About'
 
-//Javascript docs
-import Javascript from './components/docs/Javascript/1-Javascript';
+// Javascript docs
+import Javascript from './components/docs/Javascript/1-Javascript'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/docs",
-    element: <Docs />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/Javascript/*",
-    element: <Javascript />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+    {
+      path: '/docs',
+      element: <Docs />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+    {
+      path: '/Javascript/*',
+      element: <Javascript />,
+    },
+  ],
+  // {
+  //   basename: '/note-buddy', // Aggiungi questo per GitHub Pages
+  // }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
